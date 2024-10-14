d78d3a11f8c09b4a7ccb38cd003908ef4923b700
felix <felix@example.com>
2024-10-14 14:36:27 +0800

    update

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -4,7 +4,7 @@ return {
     dependencies = {
       'tpope/vim-repeat',
     },
-    config = function()
+    init = function()
       vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
       vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
       vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
